aca3bbbaff4bcaa447fd5884906f6bda64f6d00a
Rafael Ferreira <rafael680ferreira@gmail.com>
2021-08-03 18:13:21 -0300

    First steps

---
+++ src/main/java/com/wsspring/WorkshopSpringBootMongo/WorkshopSpringBootMongoApplication.java
@@ -0,0 +1,13 @@
+package com.wsspring.WorkshopSpringBootMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsspring/WorkshopSpringBootMongo/WorkshopSpringBootMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsspring.WorkshopSpringBootMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
